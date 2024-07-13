54754f38ec5e5ee4731fcc2445d630fd6db9f49a
Hugo Delahousse <hugo.delahousse@frontapp.com>
2024-07-13 09:12:06 +0200

    Initial Commit

---
+++ cobo/coboApp.swift
@@ -0,0 +1,17 @@
+//
+//  coboApp.swift
+//  cobo
+//
+//  Created by Hugo Delahousse on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct coboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
